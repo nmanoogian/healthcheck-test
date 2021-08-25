83577c6c65296fd4e08d4972d4bd37968270c9cb
Nic Manoogian <nic.manoogian@doppler.com>
2021-08-25 11:49:26 -0400

    Change polling style to awaited loop

--- check.js
+++ check.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
-function do_check() {
-  axios
+async function do_check(i) {
+  return axios
     .request("https://api.doppler.com/_/health/router", {
       headers: {
         "User-Agent": "Doppler Bot",
@@ -10,11 +10,24 @@ function do_check() {
       timeout: 10000,
     })
     .then(() => {
-      console.log(new Date(), "OK")
+      console.log(new Date(), `(${i}) OK`)
     })
     .catch(error => {
-      console.error(error)
+      console.error(i, error)
     });
 }
 
-setInterval(do_check, 5000);
+async function delay(ms) {
+  return new Promise((res) => {
+    setTimeout(res, ms);
+  });
+}
+
+async function main() {
+  while (true) {
+    await do_check(0);
+    await delay(1000);
+  }
+}
+
+main().catch(e => console.error(e))
