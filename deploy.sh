075bac47829b2223dea93c6daebf38bdcf37295b
Nic Manoogian <nic.manoogian@doppler.com>
2022-01-03 15:05:07 -0500

    Remove deploy.sh script

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-VERSION=$(git describe --always)
-sed -i "" "s/VERSION_PLACEHOLDER/$VERSION/g" check.js
-docker build . -t healthcheck-test
-docker tag healthcheck-test localhost:5000/healthcheck-test
-docker push localhost:5000/healthcheck-test
